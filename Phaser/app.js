cc5ccf6084ea73d13aa38b78b42a08497795a6ec
Gameandryan <gameandryan@gmail.com>
2018-11-01 17:10:26 +0800

    E

--- Phaser/app.js
+++ Phaser/app.js
@@ -18,10 +18,20 @@ platforms.enableBody=true;
 var ground = platforms.create(0,750, 'ground');
 ground.scale.setTo(2,2);
 ground.body.immovable = true
-var ledge = platforms.create(0, 500, 'ledge')
-ledge = 
-
+var ledge = platforms.create(0, 500, 'ground')
+ground.body.immovable = true;
+var ledge2 = platforms.create(300, 400, 'ground')
+ground.body.immovable = true;
+var style = {font: "bold 32px Arial", fill"#fff"};
+scorelabel = game.add.text(755,5, "Score:", style);
+scorenumber = game.add.text(790,5, score, style);
+lifelabel = game.add.text(10,5, "Lives:", style);
+lifenumber = game.add.text(45,5, "Score:", style);
 }
+
+
+
+
 
 function update(){
 
