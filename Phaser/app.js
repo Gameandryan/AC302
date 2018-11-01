6bcdd263216e4f1af882a16c555962414b86c4c7
Gameandryan <gameandryan@gmail.com>
2018-11-01 18:15:40 +0800

    F

--- Phaser/app.js
+++ Phaser/app.js
@@ -60,7 +60,7 @@ function update(){
 game.physics.arcade.collide(player, platforms);
 game.physics.arcade.collide(boy, platforms);
 game.physics.arcade.collide(stars, platforms);
-player.body.velocity.x = ;
+player.body.velocity.x = 0;
 if (cursors.left.isDown){
 	player.body.velocity.x = -100;
 	player.animations.play("left");
